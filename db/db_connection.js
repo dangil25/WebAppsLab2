c39853f72efecd0c3cf1d956201e7dd4ff4621c1
gerinome <danielgtravels@gmail.com>
2023-01-26 22:55:10 -0500

    f

--- db/db_connection.js
+++ db/db_connection.js
@@ -2,6 +2,7 @@ const mysql = require('mysql2');
 const dotenv = require('dotenv');
 
 dotenv.config();
+
 const dbConfig = {
     host: process.env.DB_HOST || "localhost",
     port: parseInt(process.env.DB_PORT || "3306"),
