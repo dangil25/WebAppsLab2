653ca30e19d5140e7591f4a32a2a6b71c797f302
gerinome <danielgtravels@gmail.com>
2023-01-26 19:23:03 -0500

    asd

--- db/db_connection.js
+++ db/db_connection.js
@@ -1,11 +1,11 @@
 const mysql = require('mysql2');
 
 const dbConfig = {
-    host: "<sqlclassdb-instance-1.cqjxl5z5vyvr.us-east-2.rds.amazonaws.com>",
+    host: "sqlclassdb-instance-1.cqjxl5z5vyvr.us-east-2.rds.amazonaws.com",
     port: 3306,
-    user: "<dangil25>",
-    password: "<gLpwNZjptsVA>",
-    database: "<schema>",
+    user: "dangil25",
+    password: "gLpwNZjptsVA",
+    database: "webapp_p8_2223t2_dangil25",
     connectTimeout: 10000
 }
 
