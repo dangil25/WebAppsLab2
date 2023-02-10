33f87b06ba510d1b2a419f95a9cb00add38d7c28
gerinome <danielgtravels@gmail.com>
2023-02-10 08:24:11 -0500

    lab 4.5

--- app.js
+++ app.js
@@ -1,39 +1,60 @@
 //set up the server
+const db = require("./db/db_connection");
 const express = require( "express" );
 const app = express();
 const port = 8080;
 const logger = require("morgan");
+
 app.use(logger("dev"));
 app.use(express.static(__dirname + '/public'));
+app.set("views", __dirname + "/views")
+app.set("view engine", "ejs")
 
+const read_stuff_all_sql = 
+`Select
+    id, name, arrival, a_date, d_date, locations
+From stuff`
+
+const read_item_sql=
+    `Select
+        name, arrival, a_date, d_date, locations
+    From 
+        stuff
+    Where 
+        stuff.id = ?`
 // define a route for the default home page
-// define a route for the default home page
+
 app.get( "/", ( req, res ) => {
     console.log("GET /");
-    res.sendFile( __dirname + "/views/index.html" );
+    res.render("index");
 } );
 
 // define a route for the stuff inventory page
 app.get( "/list", ( req, res ) => {
-    console.log("GET /list");
-    res.sendFile( __dirname + "/views/list.html" );
+    db.execute(read_stuff_all_sql, (error, results) => {
+        if (error){
+            res.status(500).send(error);                            
+        }else{
+            res.render('list', {inventory : results});
+        }
+    });
+    
 } );
 
-// define a route for the item detail page
-app.get( "/list/item1", ( req, res ) => {
-    console.log("GET /list/item1");
-    res.sendFile( __dirname + "/views/item1.html" );
+// define a route for the item  detail page
+app.get( "/list/item/:id", ( req, res, next) => {
+    db.execute(read_item_sql, [req.params.id], (error, results) => {
+        if (error){
+            res.status(500).send(error);                            
+        }else if (results.length === 0){
+            res.status(404).send(`No item found with id = ${req.params.id}`)
+        }else{
+            let data = results[0]
+            res.render("item", data);
+        }
+    })
 } );
 
-app.get( "/list/item2", ( req, res ) => {
-    console.log("GET /list/item2");
-    res.sendFile( __dirname + "/views/item2.html" );
-} );
-
-app.get( "/list/item3", ( req, res ) => {
-    console.log("GET /list/item3");
-    res.sendFile( __dirname + "/views/item3.html" );
-} );
 
 // start the server
 app.listen( port, () => {
