de801324a0fb52e26da5a55d5578b2b92c3640b9
gerinome <danielgtravels@gmail.com>
2023-03-10 14:40:53 -0500

    Everything

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ app.use(logger("dev"));
 app.use(express.static(__dirname + '/public'));
 app.set("views", __dirname + "/views")
 app.set("view engine", "ejs")
+app.use( express.urlencoded({ extended: false }) );
+
 
 const read_stuff_all_sql = 
 `Select
@@ -17,19 +19,56 @@ From stuff`
 
 const read_item_sql=
     `Select
-        name, arrival, a_date, d_date, locations
+        id, name, arrival, a_date, d_date, locations
     From 
         stuff
     Where 
         stuff.id = ?`
-// define a route for the default home page
+
+const delete_item_sql = `
+    delete
+    from
+        stuff
+    where     
+        id = ?
+    `
+
+const insert_item_sql = `
+    insert into stuff
+        (name, arrival, a_date, d_date)
+    values
+        (?, ?, ?, ?)
+    `
+
+const update_item_sql = `
+    UPDATE
+        stuff
+    SET
+        name = ?,
+        arrival = ?,
+        a_date = ?,
+        d_date = ?,
+        locations = ?a
+    WHERE
+        id = ?
+
+`
+
+app.post("/list/item/:id", ( req, res) => {
+    db.execute(update_item_sql, [req.body.name, req.body.arrival, req.body.a_date, req.body.d_date, req.body.locations, req.params.id], (error, results) => {
+        if (error)
+            res.status(500).send(error);
+        else {
+            res.redirect(`/list/item/${req.params.id}`);
+        }
+    });
+})
 
 app.get( "/", ( req, res ) => {
     console.log("GET /");
     res.render("index");
 } );
 
-// define a route for the stuff inventory page
 app.get( "/list", ( req, res ) => {
     db.execute(read_stuff_all_sql, (error, results) => {
         if (error){
@@ -55,6 +94,25 @@ app.get( "/list/item/:id", ( req, res, next) => {
     })
 } );
 
+app.get("/list/item/:id/delete", ( req, res) => {
+    db.execute(delete_item_sql, [req.params.id], (error, results) => {
+        if (error)
+            res.status(500).send(error);
+        else {
+            res.redirect("/list");
+        }
+    });
+})
+
+app.post("/list", ( req, res) => {
+    db.execute(insert_item_sql, [req.body.name, req.body.arrival, req.body.sdate, req.body.edate], (error, results) => {
+        if (error)
+            res.status(500).send(error);
+        else{
+            res.redirect(`/list/item/${results.insertId}`);
+        }
+    });
+})
 
 // start the server
 app.listen( port, () => {
